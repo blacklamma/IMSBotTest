3710f8d5bf2857c1067928e65d64ce06e2200fd0
blacklamma <rudradeep1998@gmail.com>
2026-07-24 23:15:23 -0700

    update constants.js

--- src/bot/constants.js
+++ src/bot/constants.js
@@ -52,7 +52,7 @@ const ims_members_channel = '1348459267442606081';
 const imc_members_channel = '1348459500281270303';
 const ima_members_channel = '1348459567335473203';
 
-const IMA_req = 240;
+const IMA_req = 280;
 const IMC_req = 400;
 const IMS_req = 480;
 
